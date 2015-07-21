a5647bc554ca9d5b7f349dd73a17c970275fb007
Ilya Belov <ibelov2@gmail.com>
2015-07-21 20:21:43 +0500

    Readable.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :prepare_for_mobile
-  #around_action :calc_sym_size #log increment symbols
+  #around_action :log_symbols #log increment symbols
   prepend_view_path 'mobile'
 
-  def calc_sym_size
+  def log_symbols
     @sym_count_val = Symbol.all_symbols
     yield
-    logger.error "Symbol increment: #{(Symbol.all_symbols - @sym_count_val).to_s}; #{controller_name}##{params[:action]}"
+    logger.error "Symbols increment: #{(Symbol.all_symbols - @sym_count_val).to_s}; #{controller_name}##{action_name}"
   end
 
 
 
   def prepare_for_mobile
     if mobile_device?
-      prepend_view_path Rails.root + 'app' + 'views' + 'mobile' #"app/views/mobile"
+      prepend_view_path "app/views/mobile"
     end
 
   end
